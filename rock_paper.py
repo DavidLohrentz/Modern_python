8208dc71f11c13e556895ebde469e9c1efe5b52d
david <david_lohrentz@yahoo.com>
2018-08-30 10:33:49 -0500

    Updated random comments

--- rock_paper.py
+++ rock_paper.py
@@ -1,4 +1,11 @@
 from random import choice
+
+human_won = choice(["Blind squirrel found a nut", "Computer lost to dumb human",
+        "Enjoy it while you can", "Dumb human got lucky"])
+
+comp_won = choice(["Computer beats the dipshit human.", "Computer wins, hahaha.",
+        "Inferior human loses again.", "How are you going to win a simple game if you can't keep ass hats out of the oval office?"])
+
 human = input("Make your selection: [r]ock, [p]aper, or [s]cissors\n> ")
 
 if human == "r" or human == "rock":
@@ -20,22 +27,22 @@ if computer == human:
     print("Tie game. Shoot again.")
 
 elif computer == "scissors" and human == "paper":
-    print(f"{computer} cuts {human}. Computer wins, sucka.")
+    print(f"{computer} cuts {human}. {comp_won}")
 
 elif computer == "paper" and human == "rock":
-    print(f"{computer} covers {human}. Computer beats the dumb human.")
+    print(f"{computer} covers {human}. {comp_won}")
 
 elif computer == "rock" and human == "scissors":
-    print(f"{computer} smashes {human}. Computer wins.")
+    print(f"{computer} smashes {human}. {comp_won}")
 
 elif human == "scissors" and computer == "paper":
-    print(f"{human} cuts {computer}. Human got lucky.")
+    print(f"{human} cuts {computer}. {human_won}.")
 
 elif human == "paper" and computer == "rock":
-    print(f"{human} covers {computer}. Stupid Human somehow wins.")
+    print(f"{human} covers {computer}. {human_won}.")
 
 elif human == "rock" and computer == "scissors":
-    print(f"{human} smashes {computer}. Human wins.")
+    print(f"{human} smashes {computer}. {human_won}.")
 
 else:
     print("You suck at this game!")
