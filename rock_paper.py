4f823a3bcf353d590bc4c2b1942c78fe9fcfa11b
david <david_lohrentz@yahoo.com>
2018-08-31 14:35:35 -0500

    Changed messages

--- rock_paper.py
+++ rock_paper.py
@@ -6,7 +6,7 @@ human_won = choice(["Blind squirrel found a nut", "Computer lost to dumb human",
 comp_won = choice(["Computer beats the dipshit human.", "Computer wins, hahaha.",
         "Inferior human loses again.", "How are you going to win a simple game if you can't keep ass hats out of the oval office?"])
 
-human = input("Make your selection: [r]ock, [p]aper, or [s]cissors\n> ")
+human = input("Make your selection: [r]ock, [p]aper, or [s]cissors\n> ").lower()
 
 if human == "r" or human == "rock":
     human = "rock"
@@ -27,22 +27,22 @@ if computer == human:
     print("Tie game. Shoot again.")
 
 elif computer == "scissors" and human == "paper":
-    print(f"{computer} cuts {human}. {comp_won}")
+    print(f"{computer.capitalize()} cuts {human}. {comp_won}")
 
 elif computer == "paper" and human == "rock":
-    print(f"{computer} covers {human}. {comp_won}")
+    print(f"{computer.capitalize()} covers {human}. {comp_won}")
 
 elif computer == "rock" and human == "scissors":
-    print(f"{computer} smashes {human}. {comp_won}")
+    print(f"{computer.capitalize()} smashes {human}. {comp_won}")
 
 elif human == "scissors" and computer == "paper":
-    print(f"{human} cuts {computer}. {human_won}.")
+    print(f"{human.capitalize()} cuts {computer}. {human_won}.")
 
 elif human == "paper" and computer == "rock":
-    print(f"{human} covers {computer}. {human_won}.")
+    print(f"{human.capitalize()} covers {computer}. {human_won}.")
 
 elif human == "rock" and computer == "scissors":
-    print(f"{human} smashes {computer}. {human_won}.")
+    print(f"{human.capitalize()} smashes {computer}. {human_won}.")
 
 else:
     print("You suck at this game!")
