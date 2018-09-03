5db926a35048ee854d37775d4d47c38d3298038b
David <david_lohrentz@yahoo.com>
2018-09-03 11:36:40 -0500

    Changed to drop-kicking punkinhead

--- loop_practice.py
+++ loop_practice.py
@@ -1,7 +1,7 @@
-get_number = int(input("How many times to kick Punkinghead in the ass?"))
+get_number = int(input("How many times to drop kick Punkinghead? "))
 get_number +=1
 
 for i in range(get_number):
-    print(f"kicking Punkinhead {i} times.")
+    print(f"Drop kicking Punkinhead {i} times.")
 
 print("Our work is done here.")
