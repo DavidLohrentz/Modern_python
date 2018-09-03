e31b23dbd6910341ce658e816a407c84fe528e19
David <david_lohrentz@yahoo.com>
2018-09-02 23:04:33 -0500

    created loop practice program

---
+++ loop_practice.py
@@ -0,0 +1,7 @@
+get_number = int(input("How many times to kick Punkinghead in the ass?"))
+get_number +=1
+
+for i in range(get_number):
+    print(f"kicking Punkinhead {i} times.")
+
+print("Our work is done here.")
