52d929942bfaa81f882492eeae89bf837c657eab
David <david_lohrentz@yahoo.com>
2018-09-09 16:24:06 -0500

    Fixed typo and added verbiage

--- loop_practice.py
+++ loop_practice.py
@@ -1,7 +1,7 @@
-get_number = int(input("How many times to drop kick Punkinghead? "))
+get_number = int(input("How many times to drop kick Punkinhead? "))
 get_number +=1
 
 for i in range(get_number):
     print(f"Drop kicking dipshit Punkinhead {i} times.")
 
-print("Our work is done here.")
+print(f"Punkinhead drop-kicked {i} times. \nOur work is done here.")
