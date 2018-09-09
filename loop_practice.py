92297c48fa996b2c083f034f31f7494504eb5c9f
David <david_lohrentz@yahoo.com>
2018-09-09 13:29:20 -0500

    Added verbiage

--- loop_practice.py
+++ loop_practice.py
@@ -2,6 +2,6 @@ get_number = int(input("How many times to drop kick Punkinghead? "))
 get_number +=1
 
 for i in range(get_number):
-    print(f"Drop kicking Punkinhead {i} times.")
+    print(f"Drop kicking dipshit Punkinhead {i} times.")
 
 print("Our work is done here.")
