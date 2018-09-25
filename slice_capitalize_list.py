ea4ee2c236407bcb204aac9c7b68b194679ca7fe
David Lohrentz <david_lohrentz@yahoo.com>
2018-09-24 19:13:21 -0500

    File to practice lists, slicing, capitalizing

---
+++ slice_capitalize_list.py
@@ -0,0 +1,7 @@
+# Practice slicing and Capitalizing words in a list
+
+PunkinHeadAdvisers = ['russian stooge', 'orange jump suit', 'wife-beater', 'traitor', 'asshole', 'dimwit', 'turd']
+# Slice the list
+print(PunkinHeadAdvisers[::2])
+# Use slicing to capitalize words in list
+print([TrumpTurds[:1].upper() + TrumpTurds[1:] for TrumpTurds in PunkinHeadAdvisers])
