416e6ba5fb478985d40d2232871d14cbff52dc86
David Lohrentz <david_lohrentz@yahoo.com>
2018-09-25 00:01:43 -0500

    Added for loop list comprehension

--- slice_capitalize_list.py
+++ slice_capitalize_list.py
@@ -1,7 +1,15 @@
 # Practice slicing and Capitalizing words in a list
-
+from random import choice
 PunkinHeadAdvisers = ['russian stooge', 'orange jump suit', 'wife-beater', 'traitor', 'asshole', 'dimwit', 'turd']
+RandomPunkinHeadFool = choice(PunkinHeadAdvisers)
+print(f"Trump's favorite adviser is a {RandomPunkinHeadFool}.")
 # Slice the list
-print(PunkinHeadAdvisers[::2])
+print(PunkinHeadAdvisers[::3])
 # Use slicing to capitalize words in list
 print([TrumpTurds[:1].upper() + TrumpTurds[1:] for TrumpTurds in PunkinHeadAdvisers])
+
+numbs = range(1,6)
+tenx = [x * 2 for x in numbs]
+# for i in numbs:
+#     print(i * 100000)
+print(tenx)
