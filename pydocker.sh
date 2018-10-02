06c42c47b52af0a77950e4d0daf461ae271e7862
David Lohrentz <david_lohrentz@yahoo.com>
2018-10-01 21:27:30 -0500

    changed name of docker container

--- pydocker.sh
+++ pydocker.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 dir=$(pwd -P)
-docker run -it --name learn-p --rm -v $dir:/var/opt python:3.7.0-alpine3.8 /bin/sh
+docker run -it --name mod_p --rm -v $dir:/var/opt python:3.7.0-alpine3.8 /bin/sh
